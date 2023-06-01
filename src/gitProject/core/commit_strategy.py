38dd679feccffed57272a332daf1bb4a1dd21847
estevam5s <estevamsouzalaureth@gamil.com>
2023-06-01 11:45:57 -0300

    :tada: initial commit

--- src/gitProject/core/commit_strategy.py
+++ src/gitProject/core/commit_strategy.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from gitProject.commands.add_command import AddCommand
-
 from gitProject.commands.push_command import PushCommand
 
 
@@ -24,3 +23,28 @@ class SingleCommitStrategy(CommitStrategy):
             add_command.execute()
             self.command_executor.execute(
                 f'git commit --date="{dates}" -m "First commit for the day!"')
+
+
+# from abc import ABC, abstractmethod
+
+
+# class CommitStrategy(ABC):
+#     @abstractmethod
+#     def commit(self, days):
+#         pass
+
+
+# class SingleCommitStrategy(CommitStrategy):
+#     def __init__(self, command_executor, commit_command):
+#         self.command_executor = command_executor
+#         self.commit_command = commit_command
+
+#     def commit(self, days):
+#         if days < 1:
+#             return
+
+#         dates = f"{days} days ago"
+#         message = f'{dates} <- this was the commit for the day!!'
+#         self.commit_command.add_commit(message)
+#         self.command_executor.execute(
+#             f'git commit --date="{dates}" -am "{message}"')
